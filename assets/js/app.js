ae4eeeeb2f7ec6f28138d7bc16b9e39b77ef5e13
Nayeem-Hasan <104249169+Nayeem-Hasan@users.noreply.github.com>
2023-01-17 16:20:06 +0600

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,23 @@
+$('.business-slider').owlCarousel({
+	loop:true,
+	nav:false,
+	dots:true,
+	items:6,
+	autoplay:false,
+	margin:76,
+});
+$('.client-area-slider').owlCarousel({
+	loop:true,
+	nav:false,
+	dots:true,
+	items:1,
+	autoplay:false,
+});
+
+// counter js====
+$('.count').counterUp({
+  delay: 10,
+  time: 10000
+});
+// counter js====
+
